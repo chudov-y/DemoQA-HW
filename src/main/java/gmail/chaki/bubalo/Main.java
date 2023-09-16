21353504a898caf1ea9b9c123c16fe117c6664b4
sound <chaki.bubalo@gmail.com>
2023-09-16 14:43:42 +0500

    Main directory deleted, bugs fixed

--- src/main/java/gmail/chaki/bubalo/Main.java
+++
@@ -1,7 +0,0 @@
-package gmail.chaki.bubalo;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/test/java/TestBox.java
+++ src/test/java/TestBox.java
@@ -2,9 +2,9 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestBox {
@@ -18,25 +18,29 @@ public class TestBox {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
 
         $("#firstName").setValue("Yuri");
         $("#lastName").setValue("Chudov");
         $("#userEmail").setValue("yuri@chudov.com");
-        $(".custom-control-label").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("August");
         $(".react-datepicker__year-select").selectOption("1987");
         $(".react-datepicker__day--0"+"04").click();
         $("#subjectsInput").setValue("English").pressEnter();
-        $("label[for='hobbies-checkbox-2']").click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/bg.jpg"));
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("bg.jpg");
         $("#currentAddress").setValue("Some address 1");
-        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
-        $("#react-select-4-input").setValue("Delhi").pressEnter();
-        //$("#submit").click();
-        executeJavaScript("document.getElementById('submit').click()");
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
 
         $(".table-responsive").shouldHave(
                 text("Yuri Chudov"),
